e7f3919d3dde2946008683dc56f7bcf094918eae
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-05-24 17:17:49 -0700

    algorithms target sum complete

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i < array.length; i++){
+    const complement = target - array[i]
+    for (let j = i + 1; j < array.length; j++){
+      if (array[j] === complement) return true
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Runtime: O(n*2)
 */
 
 /* 
   Add your pseudocode here
+  Iterate through an array of numbers
+  If the complement + number equals target 
+    Return true
+  Else return false
 */
 
 /*
   Add written explanation of your solution here
+  We iterated through each number in the array to find a complement
+  that adds up to the target number by creating two loops. The 
+  compliment number is the difference between the target number 
+  and the array number. The loops helps determine weather the addition
+  of the number in the array and the compliment is equuals to the 
+  target number. If it is then it returns true, if not then returns false. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +47,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([9, -2, 4], 4));
 }
 
 module.exports = hasTargetSum;
